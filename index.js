eff57e74161d0dbfb1b30d5f55ad782ea63f110f
tilteddev <utha.eboth@gmail.com>
2020-10-26 19:06:33 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,89 +1,54 @@
-function NetworkMod(mod) {
-	mod.game.initialize(["me","me.abnormalities", "contract"]);
-	
-	const ITEM_NOSTRUM = 200999, BUFF_NOSTRUM = [ 4020, 4021, 4022, 4023, 4025 ]
-	let interval=null, mounted=false, inContract=false, playerAbnorms={};
-
-	// hooks
-	mod.game.me.on('enter_game',() => { useNostrum() });
-	
-	mod.game.me.on('leave_game',() => { stop() });
-	mod.game.me.on('change_zone', () => { useNostrum() });
-	
-	mod.game.me.on('resurrect', () => { 
-		playerAbnorms={};
-		useNostrum(); 
-	});
-	
-	mod.game.me.on('die',() => {
-		stop();	
-	});
-	mod.game.me.on('mount',() => { mounted=true });
-	mod.game.me.on('dismount',() => { mounted=false });
-	mod.game.contract.on('begin',() => { inContract=true });
-	mod.game.contract.on('end',() => { inContract=false });
-	
-	mod.hook('S_ABNORMALITY_BEGIN', 4 ,event => {
-		if ( mod.game.me.is(event.target)) {
-			playerAbnorms[event.id]=event.id;
-		}
-	});
-	
-	mod.hook('S_ABNORMALITY_REFRESH', 2 ,event => {
-		if ( mod.game.me.is(event.target)) {
-			playerAbnorms[event.id]=event.id;
-		}
-	});
-	
-	mod.hook('S_ABNORMALITY_END', 1 ,event => {
-		if ( mod.game.me.is(event.target)) {
-			delete playerAbnorms[event.id];
-		}
-	});
-	
-	//cmd
-	mod.command.add('aps', {
-		$default() {
-			mod.settings.enabled = !mod.settings.enabled;
-			mod.command.message((mod.settings.enabled ? 'en' : 'dis') + 'abled');
-		}
-    })
-	
-	function start() {
-		stop();
-		interval = setInterval(useNostrum, 1000);
-	}
-
-	function stop() {
-		if (interval) {
-			mod.clearInterval(interval);
-			interval = null;
-		}
-	}
-	
-	function isUseNostrum() {
-		if (!mod.settings.enabled || !mod.game.isIngame || mod.game.isInLoadingScreen || !mod.game.me.alive || mounted || inContract || mod.game.me.inBattleground) {
-			return false;
-		} else {
-			let nostrumOnPlayer=[];
-			Object.keys(playerAbnorms).forEach(playerAbnorm=> {
-  				BUFF_NOSTRUM.forEach(buffNostrum => {
-					if (playerAbnorm == buffNostrum) nostrumOnPlayer.push(playerAbnorm)
-			  	});
-			})
-			return nostrumOnPlayer.length == 0;
-		}
-	}
-	
-	function useNostrum() {
-		if ( isUseNostrum() ) {
+module.exports = function AutoPrime(mod) {
+		mod.game.initialize(['me', 'me.abnormalities', 'contract']);
+		const ITEM_NOSTRUM = 200999, BUFF_NOSTRUM = 4020
+		let interval=null, isUsed=false, currentDurationInSeconds=0;
+		
+		//other hooks
+		mod.game.me.on('enter_game',() => { start() });
+		mod.game.me.on('leave_game',() => { stop() });
+		mod.game.me.on('change_zone', () => {
 			start();
-			mod.toServer('C_USE_ITEM', 3, {
-				gameId: mod.game.me.gameId,
-				id: ITEM_NOSTRUM
-			})
+		});
+		mod.game.me.on('resurrect', () => { isUsed=false; start() });
+		
+		//cmd
+		mod.command.add('aps', {
+			$default() {
+				mod.settings.enabled = !mod.settings.enabled;
+				mod.command.message((mod.settings.enabled ? 'en' : 'dis') + 'abled');
+			}
+        })
+		
+		function start() {
+			stop();
+			interval = mod.setInterval(useNostrum, 16000);
 		}
-	}
-}
 
-module.exports = { NetworkMod };
+		function stop() {
+			if (interval) {
+				clearInterval(interval);
+				interval = null;
+			}
+		}
+
+		function isUseNostrum() {
+			if (!mod.settings.enabled || !mod.game.isIngame || mod.game.isInLoadingScreen || !mod.game.me.alive || mod.game.me.mounted || mod.game.me.inBattleground) {
+				return false;
+			} else {
+				return mod.game.me.abnormalities[BUFF_NOSTRUM] ? false : true;
+			}
+		}
+		
+		function useNostrum() {
+			if ( isUseNostrum() ) {
+				isUsed=true;
+				mod.send('C_USE_ITEM', 3, {
+					gameId: mod.game.me.gameId,
+					id: ITEM_NOSTRUM
+				})
+			} else {
+				currentDurationInSeconds+=1;
+			}
+		}
+				
+}
